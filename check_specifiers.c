1aa9073329ac56f402af02beae0c833bf9374d3a
Tobi Tijani <tobi_tijani@yahoo.com>
2023-01-24 08:42:01 -0800

    Function to check conversion specifier

---
+++ check_specifiers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - produces output according to a format.
+ * @format: a character string composed of zero or more directives
+ *
+ * Return: the number of characters printed excluding the null byte
+ */
+int (*check_specifiers(const char *format))(va_list args)
+{}
